39e9a51947608ee32b63137afc8eb540746ddc92
ksemilla <kevinsemilla@gmail.com>
2024-02-21 11:54:30 +1100

    Remove unsued var

--- src/pages/items/ItemList.tsx
+++ src/pages/items/ItemList.tsx
@@ -106,7 +106,7 @@ export function ItemList() {
               <div className="mt-8">
                 <div className="flow-root">
                   <ul role="list" className="-my-6 divide-y divide-gray-200">
-                    {orderItems.map((product, idx) => (
+                    {orderItems.map((product) => (
                       <li key={product.id} className="flex py-6">
                         <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
                           <img
