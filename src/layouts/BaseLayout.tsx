525f8166234ec57c20b8577a1925e6e41251ea7d
Kevin Semilla <kevinsemilla@gmail.com>
2024-02-28 23:38:08 +1100

    Add padding to nav

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from "react-router-dom"
 export function BaseLayout() {
   return (
     <>
-      <nav className="text-center space-x-4 border-b">
+      <nav className="text-center p-2 space-x-4 border-b">
         <Link to="/users">Users</Link>
         <Link to="/items">Items</Link>
         <Link to="/school">School</Link>
